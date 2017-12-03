a77c7e994033c7eb87f25835568215a84639c0f5
m-aguirre <mpaguirre@dons.usfca.edu>
2017-12-03 15:03:37 -0800

    Settings secret key  update

--- stockvisual/settings.py
+++ stockvisual/settings.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = my_secret_key
-SECRET_KEY = S3Connection(os.environ['SECRET_KEY'])
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
