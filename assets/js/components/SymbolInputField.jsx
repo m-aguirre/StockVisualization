84cdc4f0d15498cddf47547557ac2327aa08a7d0
m-aguirre <mpaguirre@dons.usfca.edu>
2018-01-03 21:22:17 -0800

    input field handles state changes

--- assets/js/components/SymbolInputField.jsx
+++ assets/js/components/SymbolInputField.jsx
@@ -3,16 +3,30 @@ import React from 'react';
 class SymbolInputField extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      inputText: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleChange(e) {
+    this.setState({inputText: e.target.value.toUpperCase()});
+  }
+
+  handleSubmit(e) {
+    alert('symbol entered');
+    e.preventDefault();
+  }
 
   render() {
     return (
       <div className="symbol-input-field-container">
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>
             Symbol:
-            <input type="text" name="symbol" />
+            <input type="text" name="symbol" value={this.state.inputText} onChange={this.handleChange} />
           </label>
           <input type="submit" value="Submit" />
         </form>
